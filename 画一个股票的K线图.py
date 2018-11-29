76990cb4fe8275f33a27e6b901975db55d45e58c
yangwohenmai <zongxianyang@outlook.com>
2018-11-29 23:15:37 +0800

    update

--- 画一个股票的K线图.py
+++ 画一个股票的K线图.py
@@ -2,7 +2,10 @@ import tushare as ts
 import matplotlib.pyplot as plt
 import mpl_finance as mpf
 import numpy as np
-data = ts.get_k_data('002153', ktype='D', autype='qfq', start='2018-07-17', end='')
+data = ts.get_k_data('002153', ktype='M', autype='qfq', start='2018-07-17', end='')
+print(data)
+datass = ts.get_today_all()
+print(datass)
 prices = data[['open', 'high', 'low', 'close']]
 dates = data['date']
 candleData = np.column_stack([list(range(len(dates))), prices])
