7df87031cc53751bf33ae2ff3e3c0cc469ed0eff
yangwohenmai <zongxianyang@outlook.com>
2020-08-28 15:35:44 +0800

    update

--- SAR指标.py
+++ SAR指标.py
@@ -18,16 +18,16 @@ import math
 #    sys.exit(2)
 code = "002153"
 df = ts.get_k_data(code)
-df1 = df[ df['date'] > '2020-01-01']
-print(df1)
+df = df[ df['date'] > '2020-01-01']
+print(df)
 if len(df) <10:
     print(" len(df) <10 ")
     sys.exit(2)
 
-print(df1.values[:,2].tolist())
-print(df1.values.tolist())
+print(df.values[:,2].tolist())
+print(df.values.tolist())
 # SAR，Stop and Reverse，是 Welles Wilder发明的，SAR是一个基于价格/时间的指标.
-sar = talib.SAR(df1.high, df1.low)
+sar = talib.SAR(df.high, df.low)
 #print(sar[-100:])
 print(sar)
 strr = sar.values.tolist()
@@ -43,4 +43,4 @@ df.index = pd.to_datetime(df.date)
 df[['close','ma10']].plot(grid=True, title=code)
 plt.plot(df.index, sar, '.',c='black', label='sar')
 plt.legend(loc='best', shadow=True)
-#plt.show()
+plt.show()
